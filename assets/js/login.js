7ec65c269c860198c16908db90456413e2ddae38
erfannr <vanoer21@gmail.com>
2020-04-13 09:10:13 +0700

    v1.02 -tinggal edit delete

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,10 +2,10 @@ const userLogin = document.getElementById("userLogin");
 const url = "https://5e8ec152e0e7de001685f98a.mockapi.io/coba";
 
 let get = async () => {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-  };
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+};
 
 let loginUser = async (event) => {
   event.preventDefault();
@@ -15,25 +15,16 @@ let loginUser = async (event) => {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  let arr = [];
-  userData.map((data) => {
-    let datas = {
-      email: data.email,
-      password: data.password,
-    };
-    arr.push(datas);
-  });
-
-  for (let i = 0; i <= arr.length; i++) {
-    if (email === arr[i].email && password === arr[i].password) {
-      alert("Anda berhasil login");
-      window.location.href = `${window.origin}/index.html`;
-      break;
-    } else {
-      console.log("error");
-    }
+  let data = await userData.filter(
+    (e) => email === e.email && password === e.password
+  );
+  try {
+    data[0].email === email && data[0].password === password;
+    alert("Login Succsess");
+    window.location = "index.html";
+  } catch {
+    alert("Login Gagal");
   }
- 
 };
-console.log(loginUser)
+
 userLogin.addEventListener("submit", loginUser);
